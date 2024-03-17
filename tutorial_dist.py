9bc60d11e0d68d80042e429b69216246ad9e6a11
Samuel <samuelqlopes99@gmail.com>
2024-03-17 02:00:00 -0300

    tutorial

--- tutorial_dist.py
+++ tutorial_dist.py
@@ -3,6 +3,12 @@ import torch
 import torch.distributed as dist
 import torch.multiprocessing as mp
 
+"""
+    Great tutorial on blog https://blog.roboflow.com/collective-communication-distributed-systems-pytorch/
+    to learn how to manage processes and ranks, and make the collective communication functions work. Now,
+    implement those functions from scratch.
+"""
+
 def init_process(rank: int, size: int, fn, backend="gloo"):
     """Initialize distributed environment"""
     os.environ["MASTER_ADDR"] = "localhost"
@@ -29,16 +35,37 @@ def do_scatter(rank: int, size: int):
         dist.scatter(tensor, scatter_list=[], src=0, group=group)
     print(f"rank[{rank}] data = {tensor[0]}")
 
+def do_gather(rank: int, size: int):
+    group = dist.new_group(list(range(size)))
+    tensor = torch.tensor([rank], dtype=torch.float32)
+    if rank == 0:
+        tensor_list = [torch.empty(1) for i in range(size)]
+        dist.gather(tensor, gather_list=tensor_list, dst=0, group=group)
+    else:
+        dist.gather(tensor, gather_list=[], dst=0, group=group)
+    if rank == 0:
+        print(f"rank[{rank}] data = {tensor_list}")
+
+def do_broadcast(rank: int, size: int):
+    group = dist.new_group(list(range(size)))
+    if rank == 0:
+        tensor = torch.tensor([rank], dtype=torch.float32)
+    else:
+        tensor = torch.empty(1)
+    dist.broadcast(tensor, src=0, group=group)
+    # all ranks have tensor([0.]) from rank 0
+    print(f"rank[{rank}] data = {tensor}")
+
 def hello(rank: int, size: int):
     # each process will call this functions
     print(f"rank[{rank}] say hi!")
 
 if __name__ == "__main__":
-    size = 4
+    size = 6
     processes = []
     mp.set_start_method("spawn")
     for rank in range(size):
-        p = mp.Process(target=init_process, args=(rank, size, do_scatter))
+        p = mp.Process(target=init_process, args=(rank, size, do_broadcast))
         p.start()
         processes.append(p)
 
