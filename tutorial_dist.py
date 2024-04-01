9955f9b52f36011e723532c297050706e6f4c85d
Samuel <samuelqlopes99@gmail.com>
2024-03-31 22:29:45 -0300

    trying pytorch tutorial

--- tutorial_dist.py
+++ tutorial_dist.py
@@ -2,7 +2,58 @@ import os
 import torch
 import torch.distributed as dist
 import torch.multiprocessing as mp
+import torch.nn as nn
+from torch.nn.parallel import DistributedDataParallel as DDP
+import torch.optim as optim
+
+
 #from collective import scatter2
+
+def setup(rank, world_size):
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = "12355"
+    dist.init_process_group("gloo", rank=rank, world_size=world_size)
+
+def cleanup():
+    dist.destroy_process_group()
+
+class ToyModel(nn.Module):
+    def __init__(self):
+        super(ToyModel, self).__init__()
+        self.net1 = nn.Linear(10, 10)
+        self.relu = nn.ReLU()
+        self.net2 = nn.Linear(10, 5)
+
+    def forward(self, x):
+        return self.net2(self.relu(self.net1(x)))
+
+def demo_basic(rank, world_size):
+    print(f"Running basic DDP example on rank {rank}")
+    setup(rank, world_size)
+    
+    model = ToyModel().to(rank)
+    ddp_model = DDP(model, device_ids=[rank])
+    loss_fn = nn.MSELoss()
+    optimizer = optim.SGD(ddp_model.parameters(), lr=0.001)
+    optimizer.zero_grad()
+    outputs = ddp_model(torch.randn(20, 10))
+    labels = torch.randn(20, 5).to(rank)
+    loss_fn(outputs, labels).backward()
+    optimizer.step()
+
+    cleanup()
+
+def run_demo(demo_fn, world_size):
+    mp.spawn(demo_fn,
+             args=(world_size,),
+             nprocs=world_size,
+             join=True)
+
+
+
+
+
+
 
 """
     Great tutorial on blog https://blog.roboflow.com/collective-communication-distributed-systems-pytorch/
@@ -10,27 +61,42 @@ import torch.multiprocessing as mp
     implement those functions from scratch.
 """
 
-def init_process(rank: int, size: int, backend="gloo"):
-    """Initialize distributed environment"""
+"""
+
+def do_gather(rank, world_size):
+    group = dist.new_group(list(range(world_size)))
+    tensor = torch.tensor([10], dtype=torch.float32)
+    if rank == 0:
+        tensor_list = [torch.empty(1) for i in range(world_size)]
+        dist.gather(tensor, gather_list=tensor_list, dst=0, group=group)
+    else:
+        dist.gather(tensor, gather_list=[], dst=0, group=group)
+
+    if rank == 0:
+        print(f"rank[{rank}] data = {tensor_list}")
+"""
+"""
+def init_process(rank: int, size: int, fn, backend="gloo"):
+
     os.environ["MASTER_ADDR"] = "localhost"
-    os.environ["MASTER_PORT"] = "29500"
+    os.environ["MASTER_PORT"] = "23335"
     dist.init_process_group(backend=backend, rank=rank, world_size=size)
-    #fn(rank, size)
-
-def gather(tensor, root_rank):
-    gathered_tensors = [torch.empty(1) for _ in range(dist.get_world_size())]
-    dist.gather(tensor, gathered_tensors, root_rank)
-    if dist.get_rank() == root_rank:
-        return gathered_tensors[root_rank]
-
+    fn(rank, size)
 
 
 if __name__ == "__main__":
-    world_size = 4
-    init_process(rank=dist.get_rank(), size=world_size)
+    size = 4
+    processes = []
+    mp.set_start_method("spawn")
+    for rank in range(size):
+        p = mp.Process(target=init_process, args=(rank, size, do_gather))
+        p.start()
+        processes.append(p)
 
-    # tensor to be gathered
-    tensor = torch.tensor([1], dtype=torch.float32)
+    for p in processes:
+        p.join()
+    
+    
 
     # lauch multiple processes
     processes = [mp.Process(target=gather, args=(tensor, 0)) for _ in range(world_size)]
@@ -43,7 +109,7 @@ if __name__ == "__main__":
 
     if dist.get_rank() == 0:
         print(f"gathered tensor: {gathered_tensor}")
-
+"""
 
 
 """
