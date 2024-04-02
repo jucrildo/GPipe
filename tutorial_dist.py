821f4d493c396a78acc2307f1a82124124bdc6e4
Samuel <samuelqlopes99@gmail.com>
2024-04-01 22:21:29 -0300

    update tutorial

--- tutorial_dist.py
+++ tutorial_dist.py
@@ -1,4 +1,4 @@
-import os
+"""import os
 import torch
 import torch.distributed as dist
 import torch.multiprocessing as mp
@@ -6,49 +6,82 @@ import torch.nn as nn
 from torch.nn.parallel import DistributedDataParallel as DDP
 import torch.optim as optim
 
+"""
+import torch
+from torch.utils.data import Dataset, DataLoader
+from utils import MyTrainDataset
+
+class Trainer:
+    def __init__(self,
+                 model: torch.nn.module,
+                 train_data: DataLoader,
+                 optimizer: torch.optim.Optimizer,
+                 gpu_id: int,
+                 save_every: int
+    ) -> None:
+        self.gpu_id = gpu_id
+        self.model = model.to(gpu_id)
+        self.train_data = train_data
+        self.optimizer = optimizer
+        self.save_every = save_every
+
+    def _run_batch(self, source, targets):
+        self.optimizer.zero_grad() 
+        output = self.model(source)
+        loss = torch.nn.CrossEntropyLoss()(output, targets)
+        loss.backward()
+        self.optimizer.step()
+
+    def _run_epoch(self, epoch):
+        b_sz = len(next(iter(self.train_data))[0])
+        print(f"[GPU{self.gpu_id} Epoch{epoch} | batch_size{b_sz} | Steps:{len(self.train_data)}]")
+        for source, targets in self.train_data:
+            source = source.to(self.gpu_id)
+            targets = targets.to(self.gpu_id)
+            self._run_batch(source, targets)
+
+    def _save_checkpoint(self, epoch):
+        ckp = self.model.state_dict()
+        torch.save(ckp, "checkpoint.pt")
+        print(f"Epoch {epoch} | training checkpoint saved at checkpoint.pt")
+
+    def train(self, max_epochs: int):
+        for epoch in range(max_epochs):
+            self._run_epoch(epoch)
+            if epoch % self.save_every == 0:
+                self._save_checkpoint(epoch)
+
+def load_train_objs():
+        train_set = MyTrainDataset(2048)
+        model = torch.nn.Linear(20, 1)
+        optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+        return train_set, model, optimizer
+
+def prepare_dataloader(dataset: Dataset, batch_size: int): 
+    return DataLoader(
+        dataset,
+        batch_size=batch_size,
+        pin_memory=True,
+        shuffle=True
+    )
+
+def main(device, total_epochs, save_every):
+    dataset, model, optimizer = load_train_objs()
+    train_data = prepare_dataloader(dataset, batch_size=32)
+    trainer = Trainer(model, train_data, optimizer, device, save_every)
+    trainer.train(total_epochs)
+
+if __name__ == "__main__":
+    import sys
+    total_epochs = int(sys.argv[1])
+    save_every = int(sys.argv[2])
+    device = 0
+    main(device, total_epochs, save_every)
+
+
+    
 
 #from collective import scatter2
-
-def setup(rank, world_size):
-    os.environ["MASTER_ADDR"] = "localhost"
-    os.environ["MASTER_PORT"] = "12355"
-    dist.init_process_group("gloo", rank=rank, world_size=world_size)
-
-def cleanup():
-    dist.destroy_process_group()
-
-class ToyModel(nn.Module):
-    def __init__(self):
-        super(ToyModel, self).__init__()
-        self.net1 = nn.Linear(10, 10)
-        self.relu = nn.ReLU()
-        self.net2 = nn.Linear(10, 5)
-
-    def forward(self, x):
-        return self.net2(self.relu(self.net1(x)))
-
-def demo_basic(rank, world_size):
-    print(f"Running basic DDP example on rank {rank}")
-    setup(rank, world_size)
-    
-    model = ToyModel().to(rank)
-    ddp_model = DDP(model, device_ids=[rank])
-    loss_fn = nn.MSELoss()
-    optimizer = optim.SGD(ddp_model.parameters(), lr=0.001)
-    optimizer.zero_grad()
-    outputs = ddp_model(torch.randn(20, 10))
-    labels = torch.randn(20, 5).to(rank)
-    loss_fn(outputs, labels).backward()
-    optimizer.step()
-
-    cleanup()
-
-def run_demo(demo_fn, world_size):
-    mp.spawn(demo_fn,
-             args=(world_size,),
-             nprocs=world_size,
-             join=True)
-
 
 
 
